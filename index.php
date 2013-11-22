76d2b77cd4735ae18ac641480ee495aa95b62d1e
abdulbasit <abdulbasit056@gmail.com>
2013-11-22 01:50:54 -0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script language="javascript" type="text/javascript" src="jquery-1.6.2.js"></script>
+<script language="javascript" type="text/javascript" src="selector.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div style="width:auto; height:auto; float:left">
+  <div class="desi_name">Select Designation</div>
+  <div class="multi_select">
+    <ul id="select">
+      <li id="mselect_Admissions Dev. &amp; Retention - General" value="19" name="Admissions Dev. &amp; Retention - General" style="display: " class="mselect"> Admissions Dev. &amp; Retention - General </li>
+      <li id="mselect_Admissions Dev. &amp; Retention - Resid." value="20" name="Admissions Dev. &amp; Retention - Resid." style="display: " class="mselect"> Admissions Dev. &amp; Retention - Resid. </li>
+      <li id="mselect_Advisory Council" value="18" name="Advisory Council" style="display: " class="mselect"> Advisory Council </li>
+      <li id="mselect_Attendance &amp; Scholarship" value="24" name="Attendance &amp; Scholarship" style="display: " class="mselect"> Attendance &amp; Scholarship </li>
+      <li id="mselect_Budget &amp; Finance" value="25" name="Budget &amp; Finance" style="display: " class="mselect"> Budget &amp; Finance </li>
+      <li id="mselect_By-Laws" value="26" name="By-Laws" style="display: " class="mselect"> By-Laws </li>
+      <li id="mselect_Candidate Advisors (Certified by AI)" value="17" name="Candidate Advisors (Certified by AI)" style="display: " class="mselect"> Candidate Advisors (Certified by AI) </li>
+      <li id="mselect_Candidate for Designation" value="42" name="Candidate for Designation" style="display: " class="mselect"> Candidate for Designation </li>
+      <li id="mselect_Candidate Guidance General" value="21" name="Candidate Guidance General" style="display: " class="mselect"> Candidate Guidance General </li>
+      <li id="mselect_Candidate Guidance Residential" value="22" name="Candidate Guidance Residential" style="display: " class="mselect"> Candidate Guidance Residential </li>
+      <li id="mselect_Candidate Liaison to Chapter" value="23" name="Candidate Liaison to Chapter" style="display: " class="mselect"> Candidate Liaison to Chapter </li>
+      <li id="mselect_Career Opportunity" value="27" name="Career Opportunity" style="display: " class="mselect"> Career Opportunity </li>
+      <li id="mselect_Chapt. Executive Director" value="10" name="Chapt. Executive Director" style="display: " class="mselect"> Chapt. Executive Director </li>
+      <li id="mselect_Directors" value="16" name="Directors" style="display: " class="mselect"> Directors </li>
+      <li id="mselect_Education/General" value="28" name="Education/General" style="display: " class="mselect"> Education/General </li>
+      <li id="mselect_Education/Residential" value="29" name="Education/Residential" style="display: " class="mselect"> Education/Residential </li>
+      <li id="mselect_Golf" value="40" name="Golf" style="display: " class="mselect"> Golf </li>
+      <li id="mselect_Government Relations" value="30" name="Government Relations" style="display: " class="mselect"> Government Relations </li>
+      <li id="mselect_Groups" value="4" name="Groups" style="display: " class="mselect"> Groups </li>
+      <li id="mselect_Information Resources" value="31" name="Information Resources" style="display: " class="mselect"> Information Resources </li>
+      <li id="mselect_LDAC Representative" value="32" name="LDAC Representative" style="display: " class="mselect"> LDAC Representative </li>
+      <li id="mselect_LI Chapter Region IV Alternatives" value="15" name="LI Chapter Region IV Alternatives" style="display: " class="mselect"> LI Chapter Region IV Alternatives </li>
+      <li id="mselect_LI Chapter Region IV Representatives" value="14" name="LI Chapter Region IV Representatives" style="display: " class="mselect"> LI Chapter Region IV Representatives </li>
+      <li id="mselect_MAI Designated Members" value="7" name="MAI Designated Members" style="display: " class="mselect"> MAI Designated Members </li>
+      <li id="mselect_Officers" value="9" name="Officers" style="display: " class="mselect"> Officers </li>
+      <li id="mselect_Professional Practice" value="34" name="Professional Practice" style="display: " class="mselect"> Professional Practice </li>
+      <li id="mselect_Program" value="33" name="Program" style="display: " class="mselect"> Program </li>
+      <li id="mselect_Public Relations &amp; Marketing" value="35" name="Public Relations &amp; Marketing" style="display: " class="mselect"> Public Relations &amp; Marketing </li>
+      <li id="mselect_Reception" value="41" name="Reception" style="display: " class="mselect"> Reception </li>
+      <li id="mselect_Region IV Chapter Education Chair" value="11" name="Region IV Chapter Education Chair" style="display: " class="mselect"> Region IV Chapter Education Chair </li>
+      <li id="mselect_Region IV Chapter Finance Chair" value="13" name="Region IV Chapter Finance Chair" style="display: " class="mselect"> Region IV Chapter Finance Chair </li>
+      <li id="mselect_Region IV Chapter Services Chair" value="12" name="Region IV Chapter Services Chair" style="display: " class="mselect"> Region IV Chapter Services Chair </li>
+      <li id="mselect_Research &amp;Technology" value="36" name="Research &amp;Technology" style="display: " class="mselect"> Research &amp;Technology </li>
+      <li id="mselect_Seminars - General" value="37" name="Seminars - General" style="display: " class="mselect"> Seminars - General </li>
+      <li id="mselect_Seminars - Residential" value="38" name="Seminars - Residential" style="display: " class="mselect"> Seminars - Residential </li>
+      <li id="mselect_Special Seminars" value="39" name="Special Seminars" style="display: " class="mselect"> Special Seminars </li>
+    </ul>
+  </div>
+</div>
+<div class="arrows"></div>
+<div style="width:auto; height:auto; float:left;">
+<div class="desi_name">Selected Designation</div>
+<div class="multi_select">
+    <ul id="selected">
+        <li name="Affiliates" class="deselect" id="mselect_8" value="8">Affiliates<input type="hidden" value="8" id="inptselect8" name="input_select[]"></li><li name="SRA Designated Members" class="deselect" id="mselect_6" value="6">SRA Designated Members<input type="hidden" value="6" id="inptselect6" name="input_select[]"></li>                                    </ul>
+</div>    
+</div>
+</body>
+</html>
